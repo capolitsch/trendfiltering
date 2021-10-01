9991042eda9209e0d26fb49fed2515af860bd9dd
capolitsch <capolitsch@cmu.edu>
2021-10-01 18:45:24 -0400

    Debug SURE.trendfilter mathjax details

--- R/SURE.trendfilter.R
+++ R/SURE.trendfilter.R
@@ -152,6 +152,24 @@
 #' \mjeqn{K}{ascii}-fold cross validation with \mjeqn{K = 10}{ascii}. We elaborate on SURE here and refer 
 #' the reader to (link) for \mjeqn{K}{ascii}-fold cross validation. 
 #' 
+#' The SURE formula provides an unbiased estimate of the fixed-input MSPE of a 
+#' statistical estimator:
+#' \mjdeqn{\widehat{R}_0(\gamma) = \frac{1}{n}\sum_{i=1}^{n}\big(f(t_i) - \widehat{f}_0(t_i; \gamma)\big)^2 + \frac{2\overline{\sigma}^{2}\text{df}(\widehat{f}_0)}{n},}{ascii}
+#' where \mjeqn{\overline{\sigma}^{2} = n^{-1}\sum_{i=1}^{n} \sigma_i^2}{ascii} and
+#' \mjeqn{\text{df}(\widehat{f}_0)}{ascii} is defined above. A formula for the
+#' effective degrees of freedom of the trend filtering estimator is available
+#' via the generalized lasso results of (link); namely,
+#' \mjdeqn{\text{df}(\widehat{f}_0) = \mathbb{E}\left\[\text{number of knots in}\;\widehat{f}_0\right\] + k + 1.}{ascii}
+#' We then obtain our hyperparameter estimate \mjeqn{\widehat{\gamma}}{ascii} by minimizing the 
+#' following plug-in estimate for (link):
+#' \mjdeqn{\widehat{R}(\gamma) = \frac{1}{n}\sum_{i=1}^{n}\big(f(t_i) - \widehat{f}_0(t_i; \gamma)\big)^2 + \frac{2\widehat{\overline{\sigma}}^{2}\widehat{\text{df}}(\widehat{f}_0)}{n},}{ascii}
+#' where \mjeqn{\widehat{\text{df}}}{ascii} is the estimate for the effective degrees of 
+#' freedom that is obtained by replacing the expectation in (link) with 
+#' the observed number of knots, and \mjeqn{\widehat{\overline{\sigma}}^2}{ascii} is an 
+#' estimate of \mjeqn{\overline{\sigma}^2}{ascii}. If a reliable estimate of 
+#' \mjeqn{\overline{\sigma}^2}{ascii} is not available \emph{a priori}, a data-driven 
+#' estimate can be constructed (see, e.g., (link)).
+#' 
 #' @export SURE.trendfilter
 #' @author Collin A. Politsch, Ph.D., \email{collinpolitsch@@gmail.com}
 #' @seealso \code{\link{bootstrap.trendfilter}}
