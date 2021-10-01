5ed8e8d3aec22709b8fb40c5e122d6d6221c1453
capolitsch <capolitsch@cmu.edu>
2021-10-01 11:59:41 -0400

    Debug SURE.trendfilter mathjax details

--- R/SURE.trendfilter.R
+++ R/SURE.trendfilter.R
@@ -138,10 +138,9 @@
 #' Recall the DGP stated in (link) and let it be denoted by 
 #' \mjeqn{Q}{ascii} so that \mjeqn{\mathbb{E}_Q\[\cdot\]}{ascii} is the
 #' mathematical expectation with respect to the randomness of the DGP.
-#' Further, let 
-#' \mjeqn{\sigma_i^2 = \text{Var}(\epsilon_i)}{ascii}. 
+#' Further, let \mjeqn{\sigma_i^2 = \text{Var}(\epsilon_i)}{ascii}. 
 #' The fixed-input MSPE is given by
-#' \mjdeqn{R(\gamma) = \frac{1}{n}\sum_{i=1}^{n}\mathbb{E}_{Q}\Big\[(f(t_i) - \widehat{f}_0(t_i;\gamma))^2\;\Big|\;t_1,\dots,t_n\Big\]}{ascii}
+#' \mjdeqn{R(\gamma) = \frac{1}{n}\sum_{i=1}^{n}\mathbb{E}_{Q}\[(f(t_i) - \widehat{f}_0(t_i;\gamma))^2\;|\;t_1,\dots,t_n\]}{ascii}
 #'
 #' @export SURE.trendfilter
 #' @author Collin A. Politsch, Ph.D., \email{collinpolitsch@@gmail.com}
